f659c9a1075bd8c16baa360cb00b052eeec2890a
gugunantoni <43782738+gugunantoni@users.noreply.github.com>
2020-05-17 16:19:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/utsakb10117902gugunantoni/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.utsakb10117902gugunantoni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity<Public> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    {
+        Intent i= new Intent(MainActivity.this,Register2.class);
+        startActivity (i) ;
+    }
+}
